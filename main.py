19dcfa07d2c06b4860ea25c58007e65bf88e43fe
3nR-Rk2 <nkn115rr32@icloud.com>
2022-06-01 11:38:05 +0900

    Modify file extension, and make it possible to use tensorboard

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ import torch.distributed as dist
 import torch.utils.data.distributed
 from load_datasets import *
 
+from torch.utils.tensorboard import SummaryWriter
+
 parser = argparse.ArgumentParser(description='Training a ResNet on ImageNet or CIFAR-10 with Continuous Sparsification')
 parser.add_argument('--dataset', type=str, default='cifar10', help='which dataset to use(cifar10 or ImageNet)')
 parser.add_argument('--output-dir', type=str, default='./', help='output directory')
 parser.add_argument('--resume', type=bool, default=False, help='whether use saving model or not')
-parser.add_argument('--model-path', type=str, default='./model/params.pkl', help='model path')
+parser.add_argument('--model-path', type=str, default='/checkpoint.pt', help='model path under an output directory')
 parser.add_argument('--start-epoch', type=int, default='3', help='start epoch')
 parser.add_argument('--world-size', type=int, default=1, help='world_size')
 parser.add_argument('--rank',type=int, default=1, help='node rank for distributed training')
@@ -43,7 +45,12 @@ parser.add_argument('--rewind-epoch', type=int, default=2, help='epoch to rewind
 parser.add_argument('--lmbda', type=float, default=1e-8, help='lambda for L1 mask regularization (default: 1e-8)')
 parser.add_argument('--final-temp', type=float, default=200, help='temperature at the end of each round (default: 200)')
 parser.add_argument('--mask-initial-value', type=float, default=-0.01, help='initial value for mask parameters')
+
 args = parser.parse_args()
+
+run_time + times.times()
+wr_path = args.output_dir + '/runs/' + str(run_time)
+writer = SummaryWriter(wr_path)
 
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 num_devices = torch.cuda.device_count()
@@ -74,7 +81,11 @@ else:
 
 # num_class=1000 if dataset is ImageNet, num_classes=10 if dataset is cifar10.
 model = ResNet50(args.num_classes, args.mask_initial_value)
-print(model)
+
+images, labels = next(iter(train_loader))
+writer.add_graph(model, images)
+writer.close()
+
 if not args.cuda:
     print('using CPU, this will be slow')
 elif args.distributed:
@@ -93,12 +104,11 @@ elif args.which_gpu is not None:
     model = model.cuda(args.which_gpu)
 else:
     model = model.cuda()
-print(model)
 
 if args.resume:
-    input_file = args.output_dir + args.model_path
+    input_file = args.output_dir + args.model_path 
     if not args.cuda:
-        checkepoint = torch.load(input_file)
+        checkpoint = torch.load(input_file)
     else:
         loc = 'cuda:{}'.format(args.which_gpu)
         checkpoint = torch.load(args.resume, map_location=loc)
@@ -121,7 +131,9 @@ def adjust_learning_rate(optimizer, epoch):
 def compute_remaining_weights(masks):
     return 1 - sum(float((m == 0).sum()) for m in masks) / sum(m.numel() for m in masks)
 
-def train(outer_round, best_acc, epochs, output_filename='./checkpoint.pth.tar'):
+def train(outer_round, best_acc, epochs, output_filename='./checkpoint.pt'):
+    running_loss = 0.0
+    running_correct = 0
     for epoch in range(epochs):
         print('\t--------- Epoch {} -----------'.format(epoch))
         start = time.time()
@@ -130,7 +142,9 @@ def train(outer_round, best_acc, epochs, output_filename='./checkpoint.pth.tar')
         if outer_round == 0 and epoch == args.rewind_epoch: model.checkpoint()
         for optimizer in optimizers: adjust_learning_rate(optimizer, epoch)
 
+        data_num = 0
         for batch_idx, (data, target) in enumerate(train_loader):
+            data_num += data.size()[0]
             if args.cuda:
                 if args.which_gpu is not None:
                     data, target = data.cuda(device=args.which_gpu), target.cuda(device=args.which_gpu, non_blocking=True)
@@ -139,10 +153,12 @@ def train(outer_round, best_acc, epochs, output_filename='./checkpoint.pth.tar')
             for optimizer in optimizers: optimizer.zero_grad()
             output = model(data)
             pred = output.max(1)[1]
+            running_correct += (pred == target).sum().item()
             batch_correct = pred.eq(target.data.view_as(pred)).sum()
             masks = [m.mask for m in model.mask_modules]
             entries_sum = sum(m.sum() for m in masks)
             loss = F.cross_entropy(output, target) + args.lmbda * entries_sum
+            running_loss += loss.item()
             loss.backward()
             for optimizer in optimizers: optimizer.step()
         train_time = time.time()
@@ -166,6 +182,13 @@ def train(outer_round, best_acc, epochs, output_filename='./checkpoint.pth.tar')
         remaining_weights = compute_remaining_weights(masks)
         print('\t\tTemp: {:.1f}\tRemaining weights: {:.4f}\tVal acc: {:.1f}'.format(model.temp, remaining_weights, val_acc))
         print('\t\tTraining period: {:.1f}\tValidating period: {:.1f}\tSaving: {:.1f}'.format(train_pr, val_pr, save_pr))
+        writer.add_scalar('training loss', running_loss / data_num, epoch + 1)
+        writer.add_scalar('training accuracy', running_correct / data_num, epoch + 1)
+        writer.add_scalar('validation accuracy', val_acc, epoch + 1)
+        writer.close()
+        data_num = 0
+        running_loss = 0.0
+        running_correct = 0
     return best_acc
         
 def test(loader):
@@ -186,9 +209,10 @@ def test(loader):
     acc = 100. * correct.item() / total
     return acc
 
-def save_checkpoint(state, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, filename='checkpoint.pt'):
     torch.save(state, filename)
-    shutil.copyfile(filename, 'model_best.pth.tar')
+    shutil.copyfile(filename, 'model_best.pt')
+
 
 time_stump = time.time()
 new_dir_path = args.output_dir + str(time_stump)
@@ -212,7 +236,7 @@ best_acc = 0
 
 for outer_round in range(args.rounds):
     print('--------- Round {} -----------'.format(outer_round))
-    filename = new_dir_path + '/checkpoint.pth.tar'
+    filename = new_dir_path + '/checkpoint.pt'
     best_acc = train(outer_round, best_acc, args.epochs, output_filename=filename)
     model.temp = 1
     if outer_round != args.rounds-1: model.prune()
@@ -221,6 +245,6 @@ optimizers = [optim.SGD(weight_params, lr=0.00, momentum=0.0, nesterov=False, we
 model.ticket = True
 model.rewind_weights()
 best_acc = 0
-filename = new_dir_path + '/final_ticket_checkpoint.pth.tar'
+filename = new_dir_path + '/final_ticket_checkpoint.pt'
 best_acc = train(args.rounds, best_acc, epochs=1, filename=filename)
 print('final best accuracy is {}'.format(best_acc))
